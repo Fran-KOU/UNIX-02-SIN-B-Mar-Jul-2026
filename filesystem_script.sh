c8d8d3bb5d74f447334a914dc650fdc891001eba
Fran-KOU <fracunapa@uide.edu.ec>
2026-03-25 13:45:14 +0000

    mensaje

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -3,3 +3,15 @@ pwd # print working directory, imprime el directorio en el que me encuentro trab
 cd / # Se usa para cambiar el directorio al principal "/"
 ls -F # "ls" se usa para mostrar los archivos y carpetas del directorio, se usa "-F" para saber que tipo de archivo es cada uno que se ha encontrado
 ls -i # En cambio, "-i" muestra el numero de inodo de cada archivo del directorio actuala
+
+
+echo "Hola" > test.txt
+stat test.txt
+# File: test.txt
+# Size: 5               Blocks: 8          IO Block: 4096   regular file
+# Device: 0,45    Inode: 715077      Links: 1
+# Access: (0644/-rw-r--r--)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+# Access: 2026-03-25 13:40:10.798823053 +0000
+# Modify: 2026-03-25 13:40:10.798823053 +0000
+# Change: 2026-03-25 13:40:10.798823053 +0000
+# Birth: 2026-03-25 13:40:10.798823053 +0000
