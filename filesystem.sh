d881130cbd84178433ef80f4ee5f942c0b706e3a
Fran-KOU <fracunapa@uide.edu.ec>
2026-03-25 13:37:45 +0000

    filesystem

--- filesystem.sh
+++
@@ -1,1 +0,0 @@
-pwd # print working directory, imprime el directorio en el que me encuentro trabajando en este momento
---
+++ filesystem_script.sh
@@ -0,0 +1,5 @@
+pwd # print working directory, imprime el directorio en el que me encuentro trabajando en este momento
+
+cd / # Se usa para cambiar el directorio al principal "/"
+ls -F # "ls" se usa para mostrar los archivos y carpetas del directorio, se usa "-F" para saber que tipo de archivo es cada uno que se ha encontrado
+ls -i # En cambio, "-i" muestra el numero de inodo de cada archivo del directorio actuala
