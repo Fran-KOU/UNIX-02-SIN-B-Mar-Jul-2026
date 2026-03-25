0c182f6465d1f0483c0682cbcec69ea9f3481ed7
Fran-KOU <fracunapa@uide.edu.ec>
2026-03-25 13:03:41 +0000

    codigo

--- saludo.c
+++ saludo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>  // Se incluye una librería.
 
-int main() {
-    printf("Hello World!\n");
-    return 0;
-}
+int main() { // Se declara la variable int para que acepte el numeros enteros, y "main" quiere decir que todo este bloque de codigo desde la llave se ejecutará primero. Todo el bloque de codigo se separa con llaves({})
+    printf("Hello World!\n"); // Se hace print o imprimir en la terminal
+    return 0; // Se retorna 0 si todo sale bien, de lo contrario se retornará -1
+} // Aquí acaba el bloque de código.
+
+// ELF = .exe
