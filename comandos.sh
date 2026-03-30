08b384162a48f11a9f04f748fdb62308a1ed7f13
Fran-KOU <fracunapa@uide.edu.ec>
2026-03-30 13:13:27 +0000

    sudo parted

--- comandos.sh
+++ comandos.sh
@@ -3,3 +3,8 @@ sudo apt update # Update the local list of packages and the versions consulting 
 sudo apt upgrade # Download and install the most recent versions of all the packages that is already installed.
 
 sudo apt install parted # Download and install a package directly from the oificial repositories.
+
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo e # lists the partition tables of all block devices, displays a detailed device tree with file system information, 
+# inserts line breaks and a visual separator, prints the letter "e" on the screen.
+
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" # 
