1221c2b950ba1d2f15accd88797c2d0e33c03ebd
Fran-KOU <fracunapa@uide.edu.ec>
2026-03-30 13:06:59 +0000

    sudo

---
+++ comandos.sh
@@ -0,0 +1,5 @@
+sudo apt update # Update the local list of packages and the versions consulting the repositories.
+
+sudo apt upgrade # Download and install the most recent versions of all the packages that is already installed.
+
+sudo apt install parted # Download and install a package directly from the oificial repositories.
