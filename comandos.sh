300d780e0059c35157a6398e13ebb9606cbc0239
Fran-KOU <fracunapa@uide.edu.ec>
2026-03-30 13:44:46 +0000

    creating a file

--- comandos.sh
+++ comandos.sh
@@ -4,8 +4,12 @@ sudo apt upgrade # Download and install the most recent versions of all the pack
 
 sudo apt install parted # Download and install a package directly from the oificial repositories.
 
-sudo parted -l && echo -e "\n---\n" && lsblk -f && echo e # lists the partition tables of all block devices, displays a detailed device tree with file system information, 
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo e "\n---\n" # lists the partition tables of all block devices, displays a detailed device tree with file system information, 
 # inserts line breaks and a visual separator, prints the letter "e" on the screen.
 
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" # If the directory exists (-d returns true), the system has booted into UEFI mode and the command prints "UEFI", 
 # if the directory does not exist (-d returns false), the system has booted into legacy BIOS mode and the command prints "BIOS".
+
+echo "esto es un archivo" > archivo.txt 
+
+stat archivo.txt
