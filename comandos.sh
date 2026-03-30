8d5e132d12b025e8267dcab6719181042642aa57
Fran-KOU <fracunapa@uide.edu.ec>
2026-03-30 13:15:31 +0000

    BIOS

--- comandos.sh
+++ comandos.sh
@@ -7,4 +7,5 @@ sudo apt install parted # Download and install a package directly from the oific
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo e # lists the partition tables of all block devices, displays a detailed device tree with file system information, 
 # inserts line breaks and a visual separator, prints the letter "e" on the screen.
 
-[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" # 
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" # If the directory exists (-d returns true), the system has booted into UEFI mode and the command prints "UEFI", 
+# if the directory does not exist (-d returns false), the system has booted into legacy BIOS mode and the command prints "BIOS".
