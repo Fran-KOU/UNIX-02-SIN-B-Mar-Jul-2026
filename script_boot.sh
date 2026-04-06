f483636a9db5bef15d00216ad36700a1dacc262a
Fran-KOU <fracunapa@uide.edu.ec>
2026-04-06 12:34:47 +0000

    script_boot

---
+++ script_boot.sh
@@ -0,0 +1,16 @@
+@Fran-KOU ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (boot) $ cd
+# When we type "cd" it changes the personal directory
+
+@Fran-KOU ➜ ~ $ cd /
+# Same here, but here we specify which directory we want to change
+
+@Fran-KOU ➜ / $ cd /home/codespace
+# It works like the first command
+
+@Fran-KOU ➜ ~ $ cd ~
+# The same directory
+
+@Fran-KOU ➜ ~ $ cd $HOME
+[1]+  Done                    cd
+# Changes to the main directory, same function than the "~" or the "cd" command without specify.
+
