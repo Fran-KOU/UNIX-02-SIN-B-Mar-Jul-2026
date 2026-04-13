549d1ce774f364743ae1095856fd600e996c9248
Fran-KOU <fracunapa@uide.edu.ec>
2026-04-13 13:02:23 +0000

    depth

--- script.sh
+++ script.sh
@@ -41,3 +41,8 @@ ls --help
 man ls
 
 # Shows the entire manual
+
+-- depth
+
+# Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the
+# histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
