3a4d073aa906cbda675d9d6419a055c1b3a85768
Fran-KOU <fracunapa@uide.edu.ec>
2026-04-13 12:41:51 +0000

    one line command

--- script.sh
+++ script.sh
@@ -8,3 +8,12 @@ ls --all /
 
 #ls -all is the large version and has the same function.
 
+ls -a
+ls -h
+ls -l
+
+ls -l -a -h
+
+ls -l -ah
+
+# Executing each command give us different information, but if we write all the previous commands in one line it shows the information just with one line.
