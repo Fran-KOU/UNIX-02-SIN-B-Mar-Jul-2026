4d0f309f4377b054e7f9ca1c29b42f3b6ea32a59
Fran-KOU <fracunapa@uide.edu.ec>
2026-04-20 12:57:07 +0000

    Fix problema total

---
+++ prueba.sh
@@ -0,0 +1,39 @@
+    1  git clone <https://github.com/jonathanetitoo/UNIX-02-SIN-B-Mar-Jul-2026/tree/eval_p2_1_v1>
+    2  git clone https://github.com/jonathanetitoo/UNIX-02-SIN-B-Mar-Jul-2026/tree/eval_p2_1_v1
+    3  git remote add upstream https://github.com/jonathanetitoo/UNIX-02-SIN-B-Mar-Jul-2026/tree/eval_p2_1_v1
+    4  git fetch upstream
+    5  git checkout main
+    6  git clone --single-branch --branch eval_p2_1_v1 https://github.com/jonathanetitoo/UNIX-02-SIN-B-Mar-Jul-2026/tree/eval_p2_1_v1
+    7  git clone --single-branch --branch eval_p2_1_v1 https://github.com/jonathanetitoo/UNIX-02-SIN-B-Mar-Jul-2026
+    8  git clone --mirror https://github.com/jonathanetitoo/UNIX-02-SIN-B-Mar-Jul-2026
+    9  git remote -v
+   10  git fetch upstream
+   11  git add .
+   12  git commit -m "Fix problema 1: estructura de directorios corregida"
+   13  git add .
+   14  git commit -m "Fix problema 2: archivos movidos y renombrados"
+   15  git commit -a
+   16  git add
+   17  git add .
+   18  ls -l cosmos.txt
+   19  ls -l 
+   20  ls -l cosmos.txt
+   21  ls -l /bitacoras/cosmos.txt
+   22  chmod 640 cosmos.txt
+   23  ls -l /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/cosmos.txt
+   24  chmod 640 /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/cosmos.txt
+   25  ls -l /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/cosmos.txt
+   26  git add .
+   27  git commit -m "Fix problema 3: permisos numericos 640 en cosmos.txt"
+   28  chmod +x /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/galaxia.sh
+   29  ls -l /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/galaxia.sh
+   30  chmod 400 /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/galaxia.sh
+   31  ls -l /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/galaxia.sh
+   32  chmod 100 /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/galaxia.sh
+   33  ls -l /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/galaxia.sh
+   34  ls -l /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/estrella.conf
+   35  chmod o-w /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/estrella.conf
+   36  ls -l /workspaces/UNIX-02-SIN-B-Mar-Jul-2026/UNIX-02-SIN-B-Mar-Jul-2026/bitacoras/estrella.conf
+   37  git add .
+   38  git commit -m "Fix problema 4: permisos simbolicos corregidos"
+   39  history
