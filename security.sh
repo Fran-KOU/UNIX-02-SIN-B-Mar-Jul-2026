48b92e5e4382cbdf6dd86656af14e61a8079bf9b
Fran-KOU <fracunapa@uide.edu.ec>
2026-05-04 13:26:50 +0000

    groups

--- security.sh
+++ security.sh
@@ -4,3 +4,11 @@ id
 cat /etc/passwd | head -10
 # Display the first 10 lines of the information about system users.
 
+cat /etc/group | head -10
+# Display the first 10 lines of information of the groups
+
+groups
+# Display the groups
+
+groups $USER
+# Displays the security groups that the user belongs to
