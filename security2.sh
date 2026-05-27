466903f191826ae7fa5e66c081822ca1f53fab13
Fran-KOU <fracunapa@uide.edu.ec>
2026-05-27 13:07:10 +0000

    current group

--- security2.sh
+++ security2.sh
@@ -1,4 +1,6 @@
-# View the actual main group
+# Instead of the name "root" i have the name "vscode"
+
+# View the current main group
 id
 
 # Only the name of the main group
@@ -10,3 +12,14 @@ touch ~/test_grupo_heredado.txt
 
 ls -la ~/test_grupo_heredado.txt # The group is the main group of the user
 
+
+# View the current group
+
+id -gn
+
+echo "Grupo actual: $(id -gn)"
+
+# Creat a file before newgrp
+
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
