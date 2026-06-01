9ac2ea834cd4f3d81a7a18b44165a6db31d4061b
Fran-KOU <fracunapa@uide.edu.ec>
2026-06-01 13:04:06 +0000

    restrictedscript

---
+++ blackhatbash1.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+bash --version
+
+env
+
+echo ${SHELL}
+
+echo ${RANDOM}
+
+echo ${UID}
+
+echo ${OSTYPE}
+
+ps -e -f
+
+ps -ef
+
+df --human-readable
+
+#!/bin/bash -x
+# Activate the debbug mode
+
+bash blackhatbash1.sh
+
+chmod u+x blackhatbash1.sh
+
+./blackhatbash1
+
+bash -r blackhatbash1.sh
+# This will restrict some dangerous commands
+
