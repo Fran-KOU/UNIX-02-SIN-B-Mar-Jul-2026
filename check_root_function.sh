7df192c27b8c891acf6f09beceb4847b52cd4278
Fran-KOU <fracunapa@uide.edu.ec>
2026-06-10 13:25:45 +0000

    user is not root

--- check_root_function.sh
+++ check_root_function.sh
@@ -3,10 +3,10 @@
 # This function checks if the current user ID equals zero.
 
 check_if_root(){
-if [[ "${EUID}" -eq "0" ]]; then
- return 0
+if [[ "${EUID}" -eq "0" ]]; then # Effective user id equals to 0, it means "root"
+ return 0 # Success
  else
- return 1
+ return 1 # Failed
  fi
 }
 if check_if_root; then
@@ -15,4 +15,10 @@ else
  echo "User is not root!"
 fi
 
-check_if_root
+# I created another user named "estudiante", i used the command 'adduser estudiante'
+# Then i change my user to estudiante with 'su estudiante'
+
+# ┌──(estudiante㉿codespaces-59bc22)-[/workspaces/UNIX-02-SIN-B-Mar-Jul-2026]
+# └─$ bash check_root_function.sh
+# User is not root!
+
