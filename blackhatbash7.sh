71a4558f9809694a1ba37218658d7e71b745ec41
Fran-KOU <fracunapa@uide.edu.ec>
2026-06-15 13:14:41 +0000

    awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -8,4 +8,19 @@ printf "\n"
 grep -e "35.237.4.214" -e "13.66.139.0" log.txt
 printf "\n"
 
+ps | grep TTY
+printf "\n"
+
+ps | grep -i tty
+printf "\n"
+
+grep -v "35.237.4.214" log.txt
+printf "\n"
+
+grep -o "35.237.4.214" log.txt
+printf "\n"
+
+awk '{print $1}' log.txt
+printf "\n"
+
 
